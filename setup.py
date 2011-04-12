cb53296bd4cadaa9eafc51592ab51de79b35d188
Jeremy Grosser <jeremy@synack.me>
2011-04-11 17:04:51 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='infra',
+    version='0.1',
+    packages=['infra'],
+    entry_points={
+        'console_scripts': [
+            'infra = infra:main',
+        ],
+    }
+)
